d7bcf938345ec2c759b24898e74e58bfcfa1bc94
Jeff Lunt <jefflunt@gmail.com>
2012-07-03 10:34:46 -0500

    References #134 - Removed debugging output, and refactored the name of a helper variable to make it more clean what was expected, coming into that form helper method.

--- app/helpers/world_object_property_helper.rb
+++ app/helpers/world_object_property_helper.rb
@@ -1,14 +1,14 @@
 module WorldObjectPropertyHelper
-  def auto_field_for(form, world_object_property)
+  def auto_field_for(form_builder, world_object_property)
     case world_object_property.section_property.data_type
     when 'integer'
-      return form.text_field  :integer_value
+      return form_builder.text_field  :integer_value
     when 'boolean'
-      return form.check_box   :boolean_value, :class=>'center'
+      return form_builder.check_box   :boolean_value, :class=>'center'
     when 'string'
-      return form.text_field  :string_value
+      return form_builder.text_field  :string_value
     when 'text'
-      return form.text_area   :text_value
+      return form_builder.text_area   :text_value
     end
   end
 end
