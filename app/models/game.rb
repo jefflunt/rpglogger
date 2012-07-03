184ec06c82b48c2274970d9fb75f2fe239c280dc
Jeff Lunt <jefflunt@gmail.com>
2012-07-03 08:28:57 -0500

    Cleaned up list of game names, added case insensitive ordering.

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,7 +4,7 @@ class Game < ActiveRecord::Base
   validates :name, :presence => true
 
   def self.all_game_names
-    Game.find_by_sql("SELECT DISTINCT name FROM games ORDER BY name")
+    Game.find_by_sql("SELECT DISTINCT name FROM games ORDER BY LOWER(name) ASC").collect{|game| game.name}
   end
   
 end
