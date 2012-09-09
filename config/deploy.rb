a4424d757e85012488d0e9137264f6667aeaac90
Jeff Lunt <jefflunt@gmail.com>
2012-09-09 10:42:45 -0500

    Updated the deploy script to compare HEAD to the branch I specify, instead of always comparing it origin/master.

--- config/deploy.rb
+++ config/deploy.rb
@@ -77,7 +77,7 @@ namespace :deploy do
 
   desc "Make sure local git is in sync with remote."
   task :check_revision, :roles => :web do
-    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+    unless `git rev-parse HEAD` == `git rev-parse origin/#{branch}`
       puts "WARNING: HEAD is not the same as origin/master"
       puts "Run `git push` to sync changes."
       exit
