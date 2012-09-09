055ea8389991300a401141c9aa62797ba4e36ede
Jeff Lunt <jefflunt@gmail.com>
2012-09-09 10:24:39 -0500

    Updated various configs.

--- config/unicorn_init_staging.sh
+++ config/unicorn_init_staging.sh
@@ -15,7 +15,7 @@ TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/deployer/apps/rpglogger/current
 PID=$APP_ROOT/tmp/pids/unicorn.rpglogger.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E staging"
-AS_USER=deployer
+AS_USER="deployer"
 set -u
 
 OLD_PIN="$PID.oldbin"
