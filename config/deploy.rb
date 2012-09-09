b829ac07fd778a6b2824faef287a9ea0a539e93c
Jeff Lunt <jefflunt@gmail.com>
2012-09-08 21:53:24 -0500

    Capistrano for new server.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,23 +1,54 @@
 require "bundler/capistrano"
 
-server "rpglogger.com", :web, :app, :primary => true
-server "kn-pg-01", :db, :primary => true
-
 set :application, "rpglogger"
 set :user, "deployer"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "kn-aws-servers.pem")]
+
 set :scm, "git"
 set :repository, "git@github.com:normalocity/#{application}.git"
-set :branch, "master"
+set :branch do
+  puts
+  puts "Tags: " + `git tag`.split("\n").join(", ")
+  puts "Remember to push tags first: git push origin --tags"
+  ref = Capistrano::CLI.ui.ask "Tag, branch, or commit to deploy [master]: "
+  ref.empty? ? "master" : ref
+end
+
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
+task :set_roles do
+  role :app, app_server, :primary => true
+  role :web, app_server, :primary => true
+#  role :db, app_server, :primary => true
+end
+
+
+# ------= Various deployment targets =------
+# Production
+desc "deploy to staging"
+task :production do
+  set :app_server, "rpglogger.com"
+  set :rails_env, "production"
+  set_roles
+end
+
+# Staging
+desc "deploy to staging"
+task :staging do
+  set :app_server, "staging.rpglogger.com"
+  set :rails_env, "staging"
+  set_roles
+end
+
+# ------= Deploy tasks =------
 namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
