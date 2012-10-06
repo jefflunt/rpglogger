cf3b6630b757fdb0c066b4c146ffcb132e7ca484
Jeff Lunt <jefflunt@gmail.com>
2012-10-06 10:09:03 -0500

    Repaired stagign init script.

--- config/unicorn_init_staging.sh
+++ config/unicorn_init_staging.sh
@@ -13,7 +13,7 @@ set -e
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/deployer/apps/rpglogger/current
-PID=$APP_ROOT/tmp/pids/unicorn.rpglogger.pid
+PID=$APP_ROOT/../shared/pids/unicorn.rpglogger.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E staging"
 AS_USER="deployer"
 set -u
