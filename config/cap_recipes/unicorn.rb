05c30f960f9ebe03ba476d202097a5f613eb621d
Jeff Lunt <jefflunt@gmail.com>
2012-10-08 00:12:38 -0500

    Corrected some problems in the unicorn config setup.

--- config/cap_recipes/unicorn.rb
+++ config/cap_recipes/unicorn.rb
@@ -1,11 +1,10 @@
 namespace :unicorn do
   desc "Create the unicorn.rb file for configuring unicorn for this app"
   task :regenerate_config, roles: :web do
-    template "unicorn.erb", "#{current_path}/config/unicorn.rb"
-    run "#{sudo} ln -nfs #{current_path}/config/unicorn_init_#{rails_env}.sh /etc/init.d/unicorn_#{application}"
+    template "unicorn.erb", "#{deploy_to}/current/config/unicorn.rb"
+    run "#{sudo} ln -nfs #{deploy_to}/current/config/unicorn_init_#{rails_env}.sh /etc/init.d/unicorn_#{application}"
     restart
   end
-  
   after "deploy:update_code", "unicorn:regenerate_config"
   
   %w[start stop restart].each do |command|
