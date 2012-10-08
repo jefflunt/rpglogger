4d738f883210a105817852465b4515a221172ea3
Jeff Lunt <jefflunt@gmail.com>
2012-10-08 09:39:06 -0500

    Repaired a typo in the intended database.yml config situation.

--- config/deploy.rb
+++ config/deploy.rb
@@ -103,7 +103,7 @@ namespace :deploy do
     
   task :setup_config, :roles => :app do
     run "mkdir -p #{shared_path}/config"
-    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.example.yml"
     puts "Now edit the config files in #{shared_path}."
   end
   after "deploy:setup", "deploy:setup_config"
