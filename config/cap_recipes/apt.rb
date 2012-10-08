8ee2b0a40f2d34d45cbbf2bf6e80a12a9986bbf3
Jeff Lunt <jefflunt@gmail.com>
2012-10-07 19:14:28 -0500

    Added more recipes. Trying to get this to a place where I can deploy from scratch with a single command, then just do hot deploys thereafter.

--- config/cap_recipes/apt.rb
+++ config/cap_recipes/apt.rb
@@ -1,8 +1,13 @@
 namespace :apt do  
-  %w[update upgrade install remove autoremove].each do |command|
-    desc "#{command} apt-get package(s)"
+  %w[update upgrade].each do |command|
+    desc "#{command} apt-get"
     task command, roles: :web do
-      run "#{sudo} apt-get #{command} -y #{ENV["PACKAGES"]}"
+      run "#{sudo} apt-get #{command} -y"
     end
+  end
+  
+  desc "Install native library dependencies for this application"
+  task :install_app_dependencies do
+    run "#{sudo} apt-get install libpq-dev"
   end
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,16 @@ require "bundler/capistrano"
 require 'rvm/capistrano'
 
 load "config/cap_recipes/cap_helpers"
+load "config/cap_recipes/env"
 load "config/cap_recipes/apt"
 load "config/cap_recipes/nginx"
 load "config/cap_recipes/unicorn"
 
 # App deploy setting and config options
+set :provision_user, "ubuntu"
 set :application, "rpglogger"
-set :user, "deployer"
+set :user, "rpglogger"        # The name of the user who will deploy your app
+set :user_is_sudo, true       # true if you want your deploying user to be a sudo user
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 
@@ -18,7 +21,7 @@ set :rvm_install_with_sudo, true
 set :rvm_ruby_string, 'ruby-1.9.2-p320'
 set :rvm_type, :system
 
-# Setup SSH stuff to things work as expected
+# Setup SSH connection options
 ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "kn-aws-servers.pem")]
 ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
@@ -42,25 +45,53 @@ task :set_roles do
   role :web, app_server, :primary => true
 end
 
-# ------= Various deployment targets =------
-# Production
-desc "deploy to staging"
-task :production do
-  set :app_server, "rpglogger.com"
-  set :rails_env, "production"
-  set_roles
+# ------= Provision the server =------
+namespace :provision do
+  task :default do
+    create_deployment_user
+    create_convenience_aliases
+    create_authorized_keys
+    update_apt_get_packages
+    
+    setup_ruby_and_rvm
+    install_app_specific_packages
+  end
+  
+  task :create_deployment_user do
+  end
+  
+  task :create_convenience_aliases do
+  end
+  
+  task :create_authorized_keys do
+  end
+  
+  task :update_apt_get_packages do
+  end
+  
+  task :setup_ruby_and_rvm do
+  end
+  
+  task :install_app_specific_packages do
+  end
 end
 
-# Staging
-desc "deploy to staging"
-task :staging do
-  set :app_server, "staging.rpglogger.com"
-  set :rails_env, "staging"
-  set_roles
-end
-
-# ------= Deploy tasks =------
+# ------= Deploy the actual app =------
 namespace :deploy do
+  desc "Deploy a working app to a completely blank OS image"
+  task :from_scratch do
+    if deploying_to_compatible_os?
+      provision   # provision the server, with the necessary users and software dependencies
+      setup       # setup capistro stuff
+      cold        # first cold deploy
+    end
+  end
+  
+  desc "Check to see if the OS you're deploying to is supported"
+  task :check_os_compatibility do
+    run "uname -a | grep Ubuntu"
+  end
+  
   task :setup_config, :roles => :app do
     run "mkdir -p #{shared_path}/config"
     put File.read("config/database.yml"), "#{shared_path}/config/database.yml"
