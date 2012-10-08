d530e41a5b86c634745dc9c45cf7ef9d5918212b
Jeff Lunt <jefflunt@gmail.com>
2012-10-08 00:07:13 -0500

    Corrected a typo or two in the config generation template thing.

--- config/cap_recipes/cap_helpers.rb
+++ config/cap_recipes/cap_helpers.rb
@@ -1,6 +1,6 @@
 def template(from, to)
   erb = File.read(File.expand_path("../templates/#{from}", __FILE__))
-  put = ERB.new(erb).result(binding), to
+  put ERB.new(erb).result(binding), to
 end
 
 def set_default(name, *args, &block)
