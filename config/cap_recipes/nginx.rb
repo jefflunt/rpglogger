8e1808f892accaa67a35a16f575b1a63c0c8f31e
Jeff Lunt <jefflunt@gmail.com>
2012-10-08 00:08:20 -0500

    Corrected a typo in the nginx stop/start/restart task.

--- config/cap_recipes/nginx.rb
+++ config/cap_recipes/nginx.rb
@@ -18,7 +18,7 @@ namespace :nginx do
   %w[start stop restart].each do |command|
     desc "#{command} nginx"
     task command, roles: :web do
-      run "#{sudo} service nginx command"
+      run "#{sudo} service nginx #{command}"
     end
   end
 end
