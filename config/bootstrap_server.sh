9535f6d983f5c7b14a47f2d2835fe173fd581e92
Jeff Lunt <jefflunt@gmail.com>
2012-10-09 10:38:45 -0500

    Succesfully deployed with script. Made a couple of final wording changes.

--- config/bootstrap_server.sh
+++ config/bootstrap_server.sh
@@ -10,15 +10,15 @@ echo "====== rpglogger bootstrap - adventure awaits ======"
 # Package requirements
 echo ""
 echo "======> Updating OS & installed packages <======"
-echo "------> (01/$total_steps) Updating apt-get sources..."
+echo "------> (01/$total_steps) Updating package sources..."
 sudo apt-get -y update &>> /tmp/rpglogger.bootstrap.log
-echo "------> (02/$total_steps) Upgrading apt-get installed packages..."
+echo "------> (02/$total_steps) Upgrading already installed packages...patience..."
 sudo apt-get -y upgrade &>> /tmp/rpglogger.bootstrap.log
 
 # RVM + Ruby
 echo ""
 echo "======> Setting up Ruby environment <======"
-echo "------> (03/$total_steps) Installing RVM dependencies..."
+echo "------> (03/$total_steps) Installing RVM package dependencies..."
 sudo apt-get -y install git-core build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config >> /tmp/rpglogger.bootstrap.log
 echo "------> (04/$total_steps) Installing RVM in single-user mode..."
 curl -sL https://get.rvm.io | bash -s stable &>> /tmp/rpglogger.bootstrap.log
@@ -26,7 +26,7 @@ echo "------> (05/$total_steps) Reloading PATH so that RVM works..."
 source /home/deployer/.rvm/scripts/rvm &>> /tmp/rpglogger.bootstrap.log
 echo "------> (06/$total_steps) Installing RVM zlib package..."
 rvm pkg install zlib --verify-downloads 1 &>> /tmp/rpglogger.bootstrap.log
-echo "------> (07/$total_steps) Installing Ruby 1.9.2 (compiling from source takes a long time)..."
+echo "------> (07/$total_steps) Installing Ruby 1.9.2. Compiling from source...patience..."
 rvm install ruby-1.9.2-p320 &>> /tmp/rpglogger.bootstrap.log
 echo "------> (08/$total_steps) Installing 'bundler' gem..."
 gem install bundler --no-ri --no-rdoc &>> /tmp/rpglogger.bootstrap.log
@@ -39,9 +39,10 @@ sudo apt-get -y autoremove &>> /tmp/rpglogger.bootstrap.log
 
 # Done. Reboot.
 echo "------> (10/$total_steps) Done. Rebooting."
-echo "==============================================================="
+echo ""
+echo "======> NOTES <================================================"
 echo "Bootstrap finished. "
-echo "PLEASE REMEMBER to setup your ENV variables and aliases."
+echo "PLEASE REMEMBER to setup your ENV variables and aliases!"
 echo "Use 'cap [env] deploy:install' to continue with app deployment."
 echo "==============================================================="
 
