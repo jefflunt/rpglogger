46444ebb4d05640b823fefb8e2f0868cdcc25184
Jeff Lunt <jefflunt@gmail.com>
2012-10-13 21:19:37 -0500

    References #144 - Experimenting with getting S3QL working.

--- config/bootstrap_server.sh
+++ config/bootstrap_server.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -xe
 
+# NOTES
+# sudo echo "modprobe fuse" > /tmp/fuse.conf
+# sudo cp /tmp/fuse.conf /etc/modprobe.d/
+# sudo chown root:root /etc/modprobe.d/fuse.conf
+# sudo modprobe fuse
+# sudo mkdir /mnt/rpglogger-dev
+# sudo s3fs rpglogger-dev /mnt/rpglogger-dev
+
 total_steps="10"
 
 echo "====== rpglogger bootstrap - adventure awaits ======"
@@ -10,7 +18,10 @@ echo "====== rpglogger bootstrap - adventure awaits ======"
 # Package requirements
 echo ""
 echo "======> Updating OS & installed packages <======"
+echo "------> (01/$total_steps) Adding S3QL package source..."
+sudo add-apt-repository ppa:nikratio/s3ql
 echo "------> (01/$total_steps) Updating package sources..."
+
 sudo apt-get -y update &>> /tmp/rpglogger.bootstrap.log
 echo "------> (02/$total_steps) Upgrading already installed packages (SLOW)..."
 sudo apt-get -y upgrade &>> /tmp/rpglogger.bootstrap.log
@@ -19,23 +30,25 @@ sudo apt-get -y upgrade &>> /tmp/rpglogger.bootstrap.log
 echo ""
 echo "======> Setting up Ruby environment <======"
 echo "------> (03/$total_steps) Installing RVM package dependencies..."
-sudo apt-get -y install git-core build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config libmagickwand-dev &>> /tmp/rpglogger.bootstrap.log
-echo "------> (04/$total_steps) Installing RVM in single-user mode..."
-curl -sL https://get.rvm.io | bash -s stable &>> /tmp/rpglogger.bootstrap.log
-echo "------> (05/$total_steps) Reloading PATH so that RVM works..."
-source /home/deployer/.rvm/scripts/rvm &>> /tmp/rpglogger.bootstrap.log
-echo "------> (06/$total_steps) Installing RVM zlib package..."
-rvm pkg install zlib --verify-downloads 1 &>> /tmp/rpglogger.bootstrap.log
-echo "------> (07/$total_steps) Installing Ruby 1.9.2. Compiling from source (SLOW)..."
-rvm install ruby-1.9.2-p320 &>> /tmp/rpglogger.bootstrap.log
-echo "------> (08/$total_steps) Installing 'bundler' gem..."
-gem install bundler --no-ri --no-rdoc &>> /tmp/rpglogger.bootstrap.log
-
-# Cleanup
-echo ""
-echo "======> Finish & reboot <======"
-echo "------> (09/$total_steps) Cleaning up packages that are no longer needed..."
-sudo apt-get -y autoremove &>> /tmp/rpglogger.bootstrap.log
+sudo apt-get -y install git-core build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config &>> /tmp/rpglogger.bootstrap.log
+echo "------> (03/$total_steps) Installing S3QL package dependencies..."
+sudo apt-get -y install s3ql
+# echo "------> (04/$total_steps) Installing RVM in single-user mode..."
+# curl -sL https://get.rvm.io | bash -s stable &>> /tmp/rpglogger.bootstrap.log
+# echo "------> (05/$total_steps) Reloading PATH so that RVM works..."
+# source /home/deployer/.rvm/scripts/rvm &>> /tmp/rpglogger.bootstrap.log
+# echo "------> (06/$total_steps) Installing RVM zlib package..."
+# rvm pkg install zlib --verify-downloads 1 &>> /tmp/rpglogger.bootstrap.log
+# echo "------> (07/$total_steps) Installing Ruby 1.9.2. Compiling from source (SLOW)..."
+# rvm install ruby-1.9.2-p320 &>> /tmp/rpglogger.bootstrap.log
+# echo "------> (08/$total_steps) Installing 'bundler' gem..."
+# gem install bundler --no-ri --no-rdoc &>> /tmp/rpglogger.bootstrap.log
+# 
+# # Cleanup
+# echo ""
+# echo "======> Finish & reboot <======"
+# echo "------> (09/$total_steps) Cleaning up packages that are no longer needed..."
+# sudo apt-get -y autoremove &>> /tmp/rpglogger.bootstrap.log
 
 # Done. Reboot.
 echo "------> (10/$total_steps) Done. Rebooting."
